972c319b6b1f1df3acc872ea9a4821281059f982
unknown <simeon@spopov-pc.comsoft.local>
2014-03-25 11:21:02 +0200

    Mail server test

--- thanks.php
+++ thanks.php
@@ -32,7 +32,7 @@
                                 <li><a href="candles.php">Свещи</a></li>
                                 <li><a href="#">Оборудване</a></li>
                                 <li><a href="#">Курсове</a></li>
-                                <li><a href="contacts.html">Контакти</a></li>
+                                <li><a href="contacts.php">Контакти</a></li>
                             </ul>
                         </nav>
                     </div>
